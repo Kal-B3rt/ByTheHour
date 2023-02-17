f2ce88c016f9f8776ac2a6accd00c4deed1359e0
kal <kennyandmeghan@gmail.com>
2023-02-16 23:05:54 -0500

    Started preparing HomeView. Created a fetch request for projects and now working on a fetch request for items.

---
+++ ByTheHour/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  ByTheHour
+//
+//  Created by Kenny Albert on 2/16/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
--- ByTheHour/HomeView.swift
+++ ByTheHour/HomeView.swift
@@ -4,25 +4,39 @@
 //
 //  Created by Kenny Albert on 2/6/23.
 //
-
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
     static let tag: String? = "Home"
     @EnvironmentObject var dataController: DataController
     
+    ///This  fetch request fetches all of the projects that are still open and puts them in Sorting order by alphabetical.
+    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+    
+    let items: FetchRequest<Item>
+    
+    init() {
+        
+    }
+    
     var body: some View {
         NavigationStack{
-            VStack{
-                Button("Add Data") {
-                    dataController.deleteAll()
-                    try? dataController.createSampleData()
-                }
+            ScrollView{
+                
             }
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
         }
     }
 }
+
+/*
+ Button("Add Data") {
+     dataController.deleteAll()
+     try? dataController.createSampleData()
+ }
+ */
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
