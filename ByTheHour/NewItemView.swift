19757d50c2cdf330f6207709db8fa4b9681b83b6
kal <kennyandmeghan@gmail.com>
2023-04-04 16:39:40 -0400

    UI update for you NewItemView

--- ByTheHour/NewItemView.swift
+++ ByTheHour/NewItemView.swift
@@ -11,37 +11,45 @@ struct NewItemView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     
     @State private var time = ""
-    @State private var rate = 10.00
+    @State private var rate = 10.25
     @State private var title = "New Item"
     @State private var total = ""
     @State private var date = Date()
     
     var body: some View {
-        VStack{
-            VStack{
+        VStack(spacing: 0){
+            VStack(alignment: .trailing, spacing: 0){
                 Text("0:00")
-                HStack{
-                    ZStack{
+                    .font(Font.system(size:100, design: .monospaced))
+                    
+
+                HStack(alignment: .bottom){
+                    ZStack(alignment: .bottomLeading){
                         MultiplyShape1()
                             .stroke(lineWidth: 3)
                         MultiplyShape2()
                             .stroke(lineWidth: 3)
                     }
-                    .frame(width: 10, height: 10)
-                    Text("per hr")
-                    Text(String(rate.formatted(.currency(code: "USD"))))
+                    .frame(width: 15, height: 15)
+                    .padding(.bottom)
+                    
+                    Spacer()
+                    
+                    Text("$\(String(rate))")
+                        .font(Font.system(size:70, design: .monospaced))
                 }
                 NewLine()
                     .stroke(lineWidth: 4)
+                    .padding(0)
                 
             }
-            .frame(width: 300, height: 100)
-            .padding(30)
-            .background(Color.secondary)
-            .cornerRadius(10)
+            .frame(width: 300, height: 250)
+            .padding(.horizontal)
+            
+            
+            Spacer()
+            Text("New Stuff")
         }
-        Spacer()
-        Text("New Stuff")
     }
 }
     
